ccb91f5407b4d4e5b3f920315a361e1dfc68acaa
Matty <54020396+matty-ross@users.noreply.github.com>
2024-01-20 18:58:25 +0100

    [Vertex Descriptor] Loading and partially storing.

--- src/converters/vertex_descriptor/vertex_descriptor.py
+++ src/converters/vertex_descriptor/vertex_descriptor.py
@@ -22,8 +22,38 @@ class VertexDescriptor:
 
 
     def _load(self) -> None:
-        pass
+        data = io.BytesIO(self.resource_entry.data[0])
+
+        data.seek(0xC)
+        self.d3d9_vertex_descriptor.elements_count = struct.unpack('B', data.read(1))[0]
+
+        self.d3d9_vertex_descriptor.elements = []
+        for i in range(self.d3d9_vertex_descriptor.elements_count):
+            data.seek(0x10 + i * 0x10)
+            element = d3d9.Element()
+            element.stream_number = struct.unpack('B', data.read(1))[0]
+            element.vertex_stride = struct.unpack('B', data.read(1))[0]
+            element.offset = struct.unpack('<H', data.read(2))[0]
+            element.data_type = d3d9.DataType(struct.unpack('<l', data.read(4))[0])
+            element.method = d3d9.Method(struct.unpack('b', data.read(1))[0])
+            element.usage = d3d9.Usage(struct.unpack('b', data.read(1))[0])
+            element.usage_index = struct.unpack('B', data.read(1))[0]
+            element.map_index = struct.unpack('B', data.read(1))[0]
+            self.d3d9_vertex_descriptor.elements.append(element)
 
 
     def _store(self) -> None:
-        pass
+        data = io.BytesIO()
+        
+        for i, element in enumerate(self.d3d11_vertex_descriptor.elements):
+            data.seek(0x10 + i * 0x14)
+            data.write(struct.pack('b', element.semantic_name))
+            data.write(struct.pack('B', element.semantic_index))
+            data.write(struct.pack('B', element.input_slot))
+            data.write(struct.pack('b', element.input_slot_class.value))
+            data.write(struct.pack('<l', element.format.value))
+            data.write(struct.pack('<L', element.offset))
+            data.write(struct.pack('<L', element.instance_data_step_rate))
+            data.write(struct.pack('<L', element.vertex_stride))
+            
+        # TODO: write the header
