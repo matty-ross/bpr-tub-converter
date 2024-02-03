38247a7585d2ab88499233aa7aea710bc42e2261
Matty <54020396+matty-ross@users.noreply.github.com>
2024-02-03 20:09:49 +0100

    Refactor + changing resource IDs to avoid collisions.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import io
+import struct
+import random
 import tkinter, tkinter.filedialog
 
 from bnd2 import bundle_v2
@@ -9,22 +12,67 @@ from converters.texture_state.texture_state import TextureState
 from converters.material_state.material_state import MaterialState
 
 
-def convert_bundle(bundle: bundle_v2.BundleV2) -> None:
-    for resource_entry in bundle.resource_entries:
-        converter = None
-        if resource_entry.type == 0:
-            converter = Texture(resource_entry)
-        elif resource_entry.type == 10:
-            converter = VertexDescriptor(resource_entry)
-        elif resource_entry.type == 12:
-            converter = Renderable(resource_entry)
-        elif resource_entry.type == 14:
-            converter = TextureState(resource_entry)
-        elif resource_entry.type == 15:
-            converter = MaterialState(resource_entry)
-        
-        if converter is not None:
-            converter.convert()
+def convert_resource_entry(bundle: bundle_v2.BundleV2, resource_entry: bundle_v2.ResourceEntry) -> None:
+    new_id = random.randint(0x00000000, 0xFFFFFFFF)
+    
+    # Texture
+    if resource_entry.type == 0:
+        converter = Texture(resource_entry)
+        converter.convert()
+        bundle.change_resource_id(resource_entry.id, new_id)
+        return
+    
+    # Material
+    if resource_entry.type == 1:
+        data = io.BytesIO(resource_entry.data[0])
+        data.seek(0x4)
+        data.write(struct.pack('<L', new_id))
+        resource_entry.data[0] = data.getvalue()
+        bundle.change_resource_id(resource_entry.id, new_id)
+        return
+    
+    # VertexDescriptor
+    if resource_entry.type == 10:
+        converter = VertexDescriptor(resource_entry)
+        converter.convert()
+        bundle.change_resource_id(resource_entry.id, new_id)
+        return
+
+    # Renderable
+    if resource_entry.type == 12:
+        converter = Renderable(resource_entry)
+        converter.convert()
+        bundle.change_resource_id(resource_entry.id, new_id)
+        return
+
+    # TextureState
+    if resource_entry.type == 14:
+        converter = TextureState(resource_entry)
+        converter.convert()
+        bundle.change_resource_id(resource_entry.id, new_id)
+        return
+    
+    # MaterialState
+    if resource_entry.type == 15:
+        converter = MaterialState(resource_entry)
+        converter.convert()
+        bundle.change_resource_id(resource_entry.id, new_id)
+        return
+    
+    # Model
+    if resource_entry.type == 42:
+        bundle.change_resource_id(resource_entry.id, new_id)
+        return
+    
+    # GraphicsSpec
+    if resource_entry.type == 65542:
+        return
+    
+    # GraphicsSpec
+    if resource_entry.type == 65546:
+        return
+    
+    print(f"Unhandled resource entry with ID: {resource_entry.id :08X} and Type: {resource_entry.type}")
 
 
 def main() -> None:
@@ -32,10 +80,15 @@ def main() -> None:
     
     file_name = tkinter.filedialog.askopenfilename()
 
+    print(f"Converting bundle '{file_name}'...")
+
     bundle = bundle_v2.BundleV2(file_name)
     bundle.load()
-    convert_bundle(bundle)
+    for resource_entry in bundle.resource_entries:
+        convert_resource_entry(bundle, resource_entry)
     bundle.save()
+
+    print("Done")
 
 
 if __name__ == '__main__':
