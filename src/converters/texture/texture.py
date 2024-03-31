b8d1f622a2fab48f880d658832deaa4dc25d3b1f
Matty <54020396+matty-ross@users.noreply.github.com>
2024-03-31 14:37:13 +0200

    [Texture] Simplified by hardcoding non-changing values.

--- src/converters/texture/texture.py
+++ src/converters/texture/texture.py
@@ -9,77 +9,16 @@ from . import d3d11
 
 D3D9_TEXTURE_TYPE_TO_D3D11_TEXTURE_TYPE = {
     d3d9.TextureType.TEXTURE: d3d11.TextureType.TEXTURE_2D,
-    d3d9.TextureType.CUBE_TEXTURE: d3d11.TextureType.TEXTURE_1D,
+    d3d9.TextureType.CUBE_TEXTURE: d3d11.TextureType.CUBE_TEXTURE,
     d3d9.TextureType.VOLUME_TEXTURE: d3d11.TextureType.TEXTURE_3D,
 }
 
 
-D3D9_FORMAT_TO_D3D11_FORMAT = {
-    d3d9.Format.UNKNOWN: d3d11.Format.UNKNOWN,
-    # d3d9.Format.R8G8B8:
-    d3d9.Format.A8R8G8B8: d3d11.Format.B8G8R8A8_UNORM,
-    d3d9.Format.X8R8G8B8: d3d11.Format.B8G8R8X8_UNORM,
-    d3d9.Format.R5G6B5: d3d11.Format.B5G6R5_UNORM,
-    # d3d9.Format.X1R5G5B5:
-    d3d9.Format.A1R5G5B5: d3d11.Format.B5G5R5A1_UNORM,
-    d3d9.Format.A4R4G4B4: d3d11.Format.B4G4R4A4_UNORM,
-    # d3d9.Format.R3G3B2:
-    d3d9.Format.A8: d3d11.Format.A8_UNORM,
-    # d3d9.Format.A8R3G3B2:
-    # d3d9.Format.X4R4G4B4:
-    # d3d9.Format.A2B10G10R10:
-    d3d9.Format.A8B8G8R8: d3d11.Format.R8G8B8A8_UNORM,
-    # d3d9.Format.X8B8G8R8:
-    d3d9.Format.G16R16: d3d11.Format.R16G16_UNORM,
-    # d3d9.Format.A2R10G10B10:
-    d3d9.Format.A16B16G16R16: d3d11.Format.R16G16B16A16_UNORM,
-    # d3d9.Format.A8P8:
-    # d3d9.Format.P8:
-    d3d9.Format.L8: d3d11.Format.R8_UNORM,
-    # d3d9.Format.A8L8:
-    # d3d9.Format.A4L4:
-    d3d9.Format.V8U8: d3d11.Format.R8G8_SNORM,
-    # d3d9.Format.L6V5U5:
-    # d3d9.Format.X8L8V8U8:
-    d3d9.Format.Q8W8V8U8: d3d11.Format.R8G8B8A8_SNORM,
-    d3d9.Format.V16U16: d3d11.Format.R16G16_SNORM,
-    # d3d9.Format.A2W10V10U10:
-    # d3d9.Format.UYVY:
-    d3d9.Format.R8G8_B8G8: d3d11.Format.G8R8_G8B8_UNORM,
-    # d3d9.Format.YUY2:
-    d3d9.Format.G8R8_G8B8: d3d11.Format.R8G8_B8G8_UNORM,
-    d3d9.Format.DXT1: d3d11.Format.BC1_UNORM,
-    d3d9.Format.DXT2: d3d11.Format.BC2_UNORM,
-    d3d9.Format.DXT3: d3d11.Format.BC2_UNORM,
-    d3d9.Format.DXT4: d3d11.Format.BC3_UNORM,
-    d3d9.Format.DXT5: d3d11.Format.BC3_UNORM,
-    d3d9.Format.D16_LOCKABLE: d3d11.Format.D16_UNORM,
-    # d3d9.Format.D32:
-    # d3d9.Format.D15S1:
-    # d3d9.Format.D24S8:
-    # d3d9.Format.D24X8:
-    # d3d9.Format.D24X4S4:
-    d3d9.Format.D16: d3d11.Format.D16_UNORM,
-    d3d9.Format.D32F_LOCKABLE: d3d11.Format.D32_FLOAT,
-    # d3d9.Format.D24FS8:
-    # d3d9.Format.D32_LOCKABLE:
-    # d3d9.Format.S8_LOCKABLE:
-    d3d9.Format.L16: d3d11.Format.R16_UNORM,
-    # d3d9.Format.VERTEXDATA:
-    d3d9.Format.INDEX16: d3d11.Format.R16_UINT,
-    d3d9.Format.INDEX32: d3d11.Format.R32_UINT,
-    d3d9.Format.Q16W16V16U16: d3d11.Format.R16G16B16A16_SNORM,
-    # d3d9.Format.MULTI2_ARGB8:
-    d3d9.Format.R16F: d3d11.Format.R16_FLOAT,
-    d3d9.Format.G16R16F: d3d11.Format.R16G16_FLOAT,
-    d3d9.Format.A16B16G16R16F: d3d11.Format.R16G16B16A16_FLOAT,
-    d3d9.Format.R32F: d3d11.Format.R32_FLOAT,
-    d3d9.Format.G32R32F: d3d11.Format.R32G32_FLOAT,
-    d3d9.Format.A32B32G32R32F: d3d11.Format.R32G32B32A32_FLOAT,
-    # d3d9.Format.CxV8U8:
-    # d3d9.Format.A1:
-    # d3d9.Format.A2B10G10R10_XR_BIAS:
-    # d3d9.Format.BINARYBUFFER:
+D3D9_TEXTURE_FORMAT_TO_D3D11_TEXTURE_FORMAT = {
+    d3d9.TextureFormat.UNKNOWN: d3d11.TextureFormat.UNKNOWN,
+    d3d9.TextureFormat.A8R8G8B8: d3d11.TextureFormat.R8G8B8A8_UNORM,
+    d3d9.TextureFormat.DXT1: d3d11.TextureFormat.BC1_UNORM,
+    d3d9.TextureFormat.DXT5: d3d11.TextureFormat.BC3_UNORM,
 }
 
 
@@ -95,16 +34,12 @@ class Texture:
     def convert(self) -> None:
         self._load()
 
-        self.d3d11_texture.usage = d3d11.Usage.DEFAULT
         self.d3d11_texture.type = D3D9_TEXTURE_TYPE_TO_D3D11_TEXTURE_TYPE[self.d3d9_texture.type]
-        self.d3d11_texture.data_offset = self.d3d9_texture.data_offset
-        self.d3d11_texture.format = D3D9_FORMAT_TO_D3D11_FORMAT[self.d3d9_texture.format]
-        self.d3d11_texture.flags = 0
+        self.d3d11_texture.format = D3D9_TEXTURE_FORMAT_TO_D3D11_TEXTURE_FORMAT[self.d3d9_texture.format]
         self.d3d11_texture.width = self.d3d9_texture.width
         self.d3d11_texture.height = self.d3d9_texture.height
-        self.d3d11_texture.depth = self.d3d9_texture.depth
-        self.d3d11_texture.count = 1
-        self.d3d11_texture.most_detailed_mipmap_level = 0
+        self.d3d11_texture.depth = 0 if self.d3d11_texture.type == d3d11.TextureType.CUBE_TEXTURE else 1
+        self.d3d11_texture.count = 6 if self.d3d11_texture.type == d3d11.TextureType.CUBE_TEXTURE else 1
         self.d3d11_texture.mipmap_levels_count = self.d3d9_texture.mipmap_levels_count
 
         self._store()
@@ -114,15 +49,19 @@ class Texture:
         data = io.BytesIO(self.resource_entry.data[0])
 
         data.seek(0x0)
-        self.d3d9_texture.data_offset = struct.unpack('<L', data.read(4))[0]
-        data.seek(0x10)
-        self.d3d9_texture.format = d3d9.Format(struct.unpack('<l', data.read(4))[0])
+        _ = data.read(4)
+        _ = data.read(4)
+        _ = data.read(4)
+        _ = data.read(2)
+        _ = data.read(1)
+        _ = data.read(1)
+        self.d3d9_texture.format = d3d9.TextureFormat(struct.unpack('<l', data.read(4))[0])
         self.d3d9_texture.width = struct.unpack('<H', data.read(2))[0]
         self.d3d9_texture.height = struct.unpack('<H', data.read(2))[0]
         self.d3d9_texture.depth = struct.unpack('B', data.read(1))[0]
         self.d3d9_texture.mipmap_levels_count = struct.unpack('B', data.read(1))[0]
         self.d3d9_texture.type = d3d9.TextureType(struct.unpack('b', data.read(1))[0])
-        self.d3d9_texture.flags = struct.unpack('B', data.read(1))[0]
+        _ = data.read(1)
 
 
     def _store(self) -> None:
@@ -130,19 +69,19 @@ class Texture:
 
         data.seek(0x0)
         data.write(struct.pack('<L', 0))
-        data.write(struct.pack('<l', self.d3d11_texture.usage.value))
+        data.write(struct.pack('<l', 0))
         data.write(struct.pack('<l', self.d3d11_texture.type.value))
-        data.write(struct.pack('<L', self.d3d11_texture.data_offset))
+        data.write(struct.pack('<L', 0))
         data.write(struct.pack('<L', 0))
         data.write(struct.pack('<L', 0))
         data.write(struct.pack('<L', 0))
         data.write(struct.pack('<l', self.d3d11_texture.format.value))
-        data.write(struct.pack('<L', self.d3d11_texture.flags))
+        data.write(struct.pack('<L', 0))
         data.write(struct.pack('<H', self.d3d11_texture.width))
         data.write(struct.pack('<H', self.d3d11_texture.height))
         data.write(struct.pack('<H', self.d3d11_texture.depth))
         data.write(struct.pack('<H', self.d3d11_texture.count))
-        data.write(struct.pack('B', self.d3d11_texture.most_detailed_mipmap_level))
+        data.write(struct.pack('B', 0))
         data.write(struct.pack('B', self.d3d11_texture.mipmap_levels_count))
         data.write(bytes(2)) # padding
         data.write(struct.pack('<L', 0))
